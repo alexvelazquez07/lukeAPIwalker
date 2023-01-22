a73eb564c5f00264571c6fa0a9ac1eb01200e6ef
Alex Velazquez <arichard.1190@gmail.com>
2023-01-21 23:46:54 -0300

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Result from './components/Result';
+import Search from './components/Search';
+
 
 function App() {
+  const [search, setSearch] = useState({
+    category : "people",
+    id : "1",
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search search={search} setSearch={setSearch}/>
+      <Result path="/:category/:id" search={search}/>
     </div>
   );
 }
